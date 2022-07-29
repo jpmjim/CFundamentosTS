1779a0d7a090e8021fe3c0fec2b3eb5487e0bed1
Jimmy J. Pecho Malqui <jpmjim@gmail.com>
2022-07-28 21:35:19 -0500

    Union Types

---
+++ src/09-union-types.ts
@@ -0,0 +1,16 @@
+(()=>{
+  let userId: string | number;
+  userId = 1212;
+  userId = 'asasa';
+  console.log('userId', userId);
+
+  function greeting(myText: string | number) {
+   if (typeof myText === 'string') {
+     console.log(`string ${myText.toLowerCase()}`);
+   } else {
+     console.log(`number ${myText.toFixed(1)}`);
+   }
+  }
+  greeting('Nicolas');
+  greeting(12.1212121212);
+})();
